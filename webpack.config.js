1ac039d0ff4a90197c4ed1fedf83b4fe1447c171
Nick Bukovskiy <sonterixo@gmail.com>
2020-10-23 21:54:54 +0300

    Nick Slider webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [new HTMLWebpackPlugin({ template: './index.html' })],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: '3005'
+  }
+}
