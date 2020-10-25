6231921dfe1e98dce31bb55d88c1649ae8dc9a7c
Nick Bukovskiy <sonterixo@gmail.com>
2020-10-26 00:32:03 +0200

    Babel

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,16 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       }
     ]
   },
